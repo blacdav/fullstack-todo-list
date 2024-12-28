22aa209cdf68e13a108221c7d448e43c1278a329
blacdav <davidaniefoik@gmail.com>
2024-12-28 20:04:02 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,6 @@ app.delete('/:id', (req, res) => {
   });
 })
 
-app.listen(process.env.PORT, ()=> {
+app.listen(process.env.PORT, '0.0.0.0', ()=> {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
