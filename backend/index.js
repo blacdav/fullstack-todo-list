7386dba1f2037ed6736aa0309bc9d8a13b17222d
blacdav <davidaniefoik@gmail.com>
2024-12-28 19:55:41 +0100

    removed the useNewUrlParser and useUnifiedTopology, to avoid error in future

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 const Todos = require('./models/todos');
 
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB')).catch((err) => console.error(err));
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB')).catch((err) => console.error(err));
 
 const app = express();
 
